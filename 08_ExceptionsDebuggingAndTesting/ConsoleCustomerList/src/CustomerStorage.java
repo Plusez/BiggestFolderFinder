8984034ca64a7e8dfd88d5bf3fdab813136a16ef
Maxim <35666350+Plusez@users.noreply.github.com>
2020-07-07 11:34:32 +0500

    commit 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -11,17 +11,22 @@ public class CustomerStorage {
 
         if (data != null) {
             String[] components = data.split("\\s+");
-            if (!components[0].matches("[A-ZА-ЯЁ][a-zа-яё]*")) {
-                throw new IllegalArgumentException("Неверный формат имени 1");
-            } else if (!components[1].matches("[A-ZА-ЯЁ][a-zа-яё]*")) {
-                throw new IllegalArgumentException("Неверный формат имени 2");
-            } else if (!components[3].matches("(\\+)\\d{11}$")) {
-                throw new IllegalArgumentException("Неверный формат телефона");
-            } else if (!components[2].matches("[a-z0-9._-]+@[a-z0-9-]+\\.[a-z]{2,4}")) {
-                throw new IllegalArgumentException("Неверный формат почты");
-            } else {
-                String name = components[0] + " " + components[1];
-                storage.put(name, new Customer(name, components[3], components[2]));
+            if (components.length != 4) {
+                throw new IllegalArgumentException("неверный формат строки");
+            }
+            else {
+                if (!components[0].matches("[A-ZА-ЯЁ][a-zа-яё]*")) {
+                    throw new IllegalArgumentException("Неверный формат имени 1");
+                } else if (!components[1].matches("[A-ZА-ЯЁ][a-zа-яё]*")) {
+                    throw new IllegalArgumentException("Неверный формат имени 2");
+                } else if (!components[3].matches("(\\+)\\d{11}$")) {
+                    throw new IllegalArgumentException("Неверный формат телефона");
+                } else if (!components[2].matches("[a-z0-9._-]+@[a-z0-9-]+\\.[a-z]{2,4}")) {
+                    throw new IllegalArgumentException("Неверный формат почты");
+                } else {
+                    String name = components[0] + " " + components[1];
+                    storage.put(name, new Customer(name, components[3], components[2]));
+                }
             }
         } else {
             throw new IllegalArgumentException("Данные отсутствуют");
